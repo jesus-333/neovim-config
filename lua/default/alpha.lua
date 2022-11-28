2fffbd2d3ac4b23e5ecf3fa64bb713c594a4740a
Jesus <albi25.zanca@gmail.com>
2022-11-28 14:57:07 +0100

    Minor update

--- lua/default/alpha.lua
+++ lua/default/alpha.lua
@@ -36,7 +36,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Jesus Neovim Config"
 end
 
 dashboard.section.footer.val = footer()
