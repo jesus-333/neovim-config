00eef2ee29ed6be0b4e5ac8d88ddc3c1706f9bee
Jesus <albi25.zanca@gmail.com>
2023-05-12 15:42:52 +0200

    Update keymaps

--- lua/default/keymaps/navigation.lua
+++ lua/default/keymaps/navigation.lua
@@ -11,6 +11,12 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Better terminal navigation (terminal mode)
+--[[ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts) ]]
+--[[ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts) ]]
+--[[ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts) ]]
+--[[ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts) ]]
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
