5c1b5e51bbc12b04777881cfe666596c22709eff
Jesus <albi25.zanca@gmail.com>
2023-08-04 16:28:01 +0200

    Update

--- lua/default/function/execute_code.lua
+++ lua/default/function/execute_code.lua
@@ -1,14 +1,3 @@
-vim.api.nvim_create_user_command("ReloadConfig", function()
-  for name,_ in pairs(package.loaded) do
-    if name:match('^default') and not name:match('nvim-tree') then
-      package.loaded[name] = nil
-    end
-  end
-
-  dofile(vim.env.MYVIMRC)
-  vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
-end, {})
-
 function _G.execute_python()
 	local filename = vim.api.nvim_buf_get_name(0)
 	if string.match(filename, ".py") then
--- lua/default/function/various.lua
+++ lua/default/function/various.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_user_command("ReloadConfig", function()
+  for name,_ in pairs(package.loaded) do
+    if name:match('^default') and not name:match('nvim-tree') then
+      package.loaded[name] = nil
+    end
+  end
+
+  dofile(vim.env.MYVIMRC)
+  vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+end, {})
+
+
 function _G.concat_string_array(string_array)
 	local concat_string = ""
 	for i in pairs(string_array) do
