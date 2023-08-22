b762491773f9a65713cc666a9862a5fc689d6388
Jesus <albi25.zanca@gmail.com>
2023-08-22 18:33:09 +0200

    Test on possible tmux integration

--- lua/default/function/tmux_integration.lua
+++ lua/default/function/tmux_integration.lua
@@ -1,3 +1,9 @@
 function _G.send_command_tmux_panes(n_panes)
 	vim.cmd("!tmux send-keys -t 1 ls Enter")
 end
+
+function _G.send_yanked_text_ipython(n_panes)
+	command = "!tmux send-keys -t "..n_panes
+	command = command .. " \\%paste Enter"
+	vim.cmd(command)
+end
