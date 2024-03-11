6f50cd137a7b74c8db6ca5e857449c60db7d8a89
Jesus <albi25.zanca@gmail.com>
2024-03-11 11:01:53 +0100

    Minor update (fish config and various.lua)

--- other/config.fish
+++ other/config.fish
@@ -1,3 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
