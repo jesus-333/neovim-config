dc3f0577a084325e15dc0f623228fc7d5b96674f
Jesus <albi25.zanca@gmail.com>
2024-10-11 10:23:13 +0200

    Add taplo lsp (for toml files)

--- lua/default/lsp/settings/rust_analyzer.lua
+++ lua/default/lsp/settings/rust_analyzer.lua
@@ -1,9 +1,9 @@
 return {
-  settings = {
-    ['rust-analyzer'] = {
-      diagnostics = {
-        enable = true;
-      }
-    }
-  }
+	settings = {
+		['rust-analyzer'] = {
+			diagnostics = {
+				enable = true;
+			}
+		}
+	}
 }
---
+++ lua/default/lsp/settings/taplo.lua
@@ -0,0 +1,8 @@
+return {
+	settings = {
+		cmd = { "taplo", "lsp", "stdio" },
+		filetypese = { "toml" },
+		root_dir = root_pattern(".git"),
+		single_file_support = true
+	}
+}
