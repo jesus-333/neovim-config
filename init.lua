75ca59fe74ee01bba755273a5eeeae1991576e45
jesus <albi25.zanca@gmail.com>
2025-10-22 09:21:57 +0200

    Minor update

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ require "default.indentline"		-- Show Vertical line corresponding to the various
 require "default.surround"			-- Surround stuff with brackets et similia
 require "default.project"			-- Setup of the project plugin
 require "default.trouble"			-- Trouble plugin (list of lsp notes/error in the files)
-require "default.dressing"			-- Ui Extension
+require "default.dressing"			-- UI Extension
 require "default.noice"				-- Replace cmd
 require "default.notify"
 require "default.leap"				-- Navigation plugin
--- lua/default/telescope.lua
+++ lua/default/telescope.lua
@@ -27,27 +27,28 @@ telescope.setup {
 				["<C-p>"] = actions.cycle_history_prev,
 
 				-- Move through results search
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"]  = actions.move_selection_next,
+				["<C-k>"]  = actions.move_selection_previous,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"]   = actions.move_selection_previous,
 
 				["<C-c>"] = actions.close,
-
-				["<Down>"] = actions.move_selection_next,
-				["<Up>"] = actions.move_selection_previous,
 
 				["<CR>"] = actions.select_default,
 				["<C-x>"] = actions.select_horizontal,
 				["<C-v>"] = actions.select_vertical,
 				["<C-t>"] = actions.select_tab,
-
+				
+				-- Scroll text in the preview
 				["<C-u>"] = actions.preview_scrolling_up,
 				["<C-d>"] = actions.preview_scrolling_down,
-
-				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageUp>"]   = actions.results_scrolling_up,
 				["<PageDown>"] = actions.results_scrolling_down,
 
+				-- Select entries
 				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
 				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+
 				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 				["<C-l>"] = actions.complete_tag,
